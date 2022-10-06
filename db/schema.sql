f2f0af2602a54a8ba7b4b79136c2294ae03c744c
jenics <jenireecastro@gmail.com>
2022-10-06 13:23:37 -0500

    updated files

--- db/schema.sql
+++ db/schema.sql
@@ -11,8 +11,8 @@ CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INTEGER,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
+    department_id INTEGER
+   
 );
 
 CREATE TABLE employees (
@@ -20,6 +20,6 @@ CREATE TABLE employees (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
-    manager_id INTEGER REFERENCES employees,
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
+    manager_id INTEGER REFERENCES employees
+  
 );
